2b90a145ff13906792929e1980ad0d039387013e
王业强 <2021014207@nwafu.edu.cn>
2024-11-06 23:55:26 +0800

    the first commit

---
+++ Result/20241106_212055/train.py
@@ -0,0 +1,342 @@
+# -*- coding:utf-8 -*-
+# 作者: 王业强__
+# 日期: 2024-08-15
+# 声明: Welcome my coding environments!
+
+import time
+import json
+import shutil
+from tqdm import tqdm
+from numpy import interp
+from sklearn.manifold import TSNE
+from torch_geometric.data import Data
+from sklearn.decomposition import PCA
+from torch.optim.lr_scheduler import StepLR
+from sklearn.metrics import precision_recall_curve
+from sklearn.model_selection import StratifiedKFold
+
+from model import *
+from utils import *
+import warnings
+warnings.filterwarnings('ignore')
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f'Using device: {device}')
+
+# 数据预处理
+features, edge_index, _, all_samples, labels = load_data()
+features = features.to(device)
+edge_index = edge_index.to(device)
+
+# 构建图数据对象
+data = Data(x=features, edge_index=edge_index, y=labels)
+
+# 将Tensor转换为numpy数组
+samples_numpy = all_samples.numpy()
+labels_numpy = labels.numpy()
+
+all_samples = all_samples.to(device)
+labels = labels.to(device)
+
+# 训练函数
+def train(myModel, epochs, learning_rate, save_path, save_img_path, save_csv_path):
+    start_time = time.time()
+    print("feature_size: ", data.x.shape)
+    epochs = epochs
+    learning_rate = learning_rate
+
+    random_state = 41
+
+    # 五折交叉验证
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=random_state)
+
+    # 保存当前运行的 python文件和用到的 python文件
+    current_file_path = os.path.abspath(__file__)
+    current_file_name = os.path.basename(current_file_path)
+    target_file_path = save_path + current_file_name
+    model_path = "model.py"
+    target_model_path = save_path + model_path
+    function_path = "utils.py"
+    target_function_path = save_path + function_path
+
+    # 复制当前运行的文件到目标目录
+    shutil.copy2(current_file_path, target_file_path)
+    shutil.copy2(model_path, target_model_path)
+    shutil.copy2(function_path, target_function_path)
+
+    tprs = []
+    mean_fpr = np.linspace(0, 1, 100)
+
+    # 准备存储交叉验证过程中的评价指标
+    metrics_per_fold = {
+        'epoch_losses': [],  # 存储每一折的每个epoch的损失
+        'epoch_accuracies': [],
+        'accuracies': [], 'precisions': [], 'recalls': [], 'f1s': [], 'aucs': [],  # 存储每一折结束后的评价指标
+        'roc_aucs': [],
+        'fprs': [],
+        'tprs': [],
+        'mean_fpr': [],
+        'mean_tpr': [],
+        'tprs_': [],
+        'std_tpr': [],
+        'pr_precisions': [],
+        'pr_recalls': [],
+        'pr_aucs': []
+    }
+
+    result = []
+    for fold, (train_idx, val_idx) in enumerate(skf.split(samples_numpy, labels_numpy)):
+
+        # 每一折重新初始化模型
+        model = myModel(num_features=features.shape[1]).to(device)  # 创建新的模型实例
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+        criterion = nn.BCEWithLogitsLoss()
+
+        scheduler = StepLR(optimizer, step_size=400, gamma=0.5)
+
+        # 抽取训练集和验证集的样本和标签
+        train_samples = all_samples[train_idx]
+        val_samples = all_samples[val_idx]
+        train_labels = labels[train_idx]
+        val_labels = labels[val_idx]
+
+        # 根据划分的训练集得出训练的关联关系单向边
+        train_edge_index = train_samples[labels[train_idx] == 1].T
+        val_edge_index = val_samples[labels[val_idx] == 1].T
+
+        # if (fold == 0):
+        #     print("\nBefore add M/D edge_index:")
+        #     print("train_edge_index.shape:", train_edge_index.shape)
+        #     print("val_edge_index.shape:", val_edge_index.shape)
+
+        # 根据 train_edge_index 重新算 M、D 的边并加入边集
+        M, D = get_MD(train_edge_index.cpu())
+        M_edge_index = torch.nonzero(M, as_tuple=False).t().contiguous()
+        D_edge_index = torch.nonzero(D, as_tuple=False).t().contiguous()
+        D_edge_index += torch.tensor(812)
+        # 计算 train_edge_index
+        train_edge_index_else = train_edge_index.flip(dims=[0])
+        train_edge_index = torch.cat([train_edge_index, train_edge_index_else], dim=1)
+        train_edge_index = torch.cat([M_edge_index, D_edge_index, train_edge_index.cpu()], dim=1)
+        train_edge_index = torch.tensor(train_edge_index, dtype=torch.long).to(device)
+
+        # if (fold == 0):
+        #     print("\ntrain_samples.shape:", train_samples.shape)
+        #     print("val_samples.shape:", val_samples.shape)
+        #     print("\nAfter add M/D edge_index:")
+        #     print("M_edge_index.shape_train: ", M_edge_index.shape)
+        #     print("D_edge_index.shape_train: ", D_edge_index.shape)
+        #     print("train_edge_index.shape:", train_edge_index.shape)
+
+        # 验证集
+        M, D = get_MD(val_edge_index.cpu())
+        M_edge_index = torch.nonzero(M, as_tuple=False).t().contiguous()
+        D_edge_index = torch.nonzero(D, as_tuple=False).t().contiguous()
+        D_edge_index += torch.tensor(812)
+        # 拼接验证集的双向边
+        val_edge_index_else = val_edge_index.flip(dims=[0])
+        val_edge_index = torch.cat([val_edge_index, val_edge_index_else], dim=1)
+        val_edge_index = torch.cat([M_edge_index, D_edge_index, val_edge_index.cpu()], dim=1)
+        val_edge_index = torch.tensor(val_edge_index, dtype=torch.long).to(device)
+
+        # if (fold == 0):
+        #     print("M_edge_index.shape_val: ", M_edge_index.shape)
+        #     print("D_edge_index.shape_val: ", D_edge_index.shape)
+        #     print("val_edge_index.shape:", val_edge_index.shape)
+        #
+        #     # df = pd.DataFrame(train_edge_index.cpu().T)
+        #     # df.to_csv('datasets/train_edge_index_all.csv',index = False)
+
+        epoch_losses = []  # 存储当前折的损失
+        epoch_accuracies = []  # 存储当前折的acc
+
+        for epoch in tqdm(range(epochs), desc=f'fold_{fold + 1}', total=epochs, ncols=50):
+
+            model.train()
+            optimizer.zero_grad()
+            out, embedding, edge_embedding, attention_out, out_edge_logits = model(
+                data.x, train_edge_index.to(device), train_samples)
+            out = out.squeeze()
+            train_out = out_edge_logits.squeeze().cpu()
+
+            loss = criterion(train_out, train_labels.cpu())
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            train_preds = (torch.sigmoid(train_out) > 0.5).float()
+
+            train_accuracy = accuracy_score(train_labels.cpu().numpy(), train_preds.detach().numpy())
+            epoch_losses.append(loss.item())
+            epoch_accuracies.append(train_accuracy)
+
+            # 绘制最后一轮分类效果的embedding
+            if (epoch == epochs - 1):
+                positive_num = int(torch.sum(train_labels).item())
+                pca = PCA(n_components=2)
+                pca_result = pca.fit_transform(edge_embedding.cpu().detach().numpy())
+
+                # 使用t-SNE进行进一步降维
+                tsne = TSNE(n_components=2)
+                tsne_result = tsne.fit_transform(pca_result)
+
+                # 为miRNA和疾病节点指定两种不同的颜色
+                plt.figure(figsize=(10, 8))
+                plt.scatter(tsne_result[:positive_num, 0], tsne_result[:positive_num, 1], color='red', label='positive')
+                plt.scatter(tsne_result[positive_num:, 0], tsne_result[positive_num:, 1], color='blue',
+                            label='negative')
+
+                plt.title('t-SNE edge_embedding of the PMDGGM model')
+                plt.xlabel('Principal Component 1')
+                plt.ylabel('Principal Component 2')
+                plt.legend()  # 添加图例
+                # 保存图像
+                if (fold == 0):
+                    plt.savefig(save_img_path + 'embedding.png')
+                plt.show()
+
+        # 每折训练结束后的评估
+        model.eval()
+        with torch.no_grad():
+            out, embedding, edge_embedding, attention_out, out_edge_logits = model(
+                data.x, val_edge_index.to(device), val_samples)
+            out = out.squeeze()
+            val_out = out_edge_logits.squeeze().cpu()
+
+            val_preds = (torch.sigmoid(val_out) > 0.5).float()
+
+            val_labels = val_labels.cpu()
+
+            # ROC
+            roc_auc = roc_auc_score(val_labels.numpy(), val_out.numpy())
+            fpr, tpr, thresholds = roc_curve(val_labels.numpy(), val_out.numpy())
+            tprs.append(interp(mean_fpr, fpr, tpr))
+            tprs[-1][0] = 0.0
+
+            pr_precision, pr_recall, _ = precision_recall_curve(val_labels.numpy(), val_out.numpy())
+            pr_auc = auc(pr_recall, pr_precision)
+
+            accuracy = accuracy_score(val_labels.numpy(), val_preds.numpy())
+            precision = precision_score(val_labels.numpy(), val_preds.numpy())
+            recall = recall_score(val_labels.numpy(), val_preds.numpy())
+            f1 = f1_score(val_labels.numpy(), val_preds.numpy())
+
+            save_pred_path = save_path + 'pred' + '/'
+            M = embedding[:812, :]
+            D = embedding[812:, :]
+            M_D = torch.matmul(M, D.t())
+            df = pd.DataFrame(M_D.cpu())
+            df.to_csv('{}pred_scores_{}.csv'.format(save_pred_path, fold + 1))
+
+            metrics_per_fold['roc_aucs'].append(roc_auc)
+            metrics_per_fold['fprs'].append(fpr)
+            metrics_per_fold['tprs'].append(tpr)
+
+            metrics_per_fold['pr_precisions'].append(pr_precision)
+            metrics_per_fold['pr_recalls'].append(pr_recall)
+            metrics_per_fold['pr_aucs'].append(pr_auc)
+
+            metrics_per_fold['epoch_losses'].append(epoch_losses)
+            metrics_per_fold['epoch_accuracies'].append(epoch_accuracies)
+            metrics_per_fold['accuracies'].append(accuracy)
+            metrics_per_fold['precisions'].append(precision)
+            metrics_per_fold['recalls'].append(recall)
+            metrics_per_fold['f1s'].append(f1)
+
+            print(f"Fold {fold + 1}")
+            print(f"Accuracy: {accuracy:.5f}, "
+                  f"Precision: {precision:.5f},"f" Recall: {recall:.5f}, "
+                  f"F1: {f1:.5f}, ROCAUC: {roc_auc:.5f}, PRAUC: {pr_auc:.5f}")
+
+            fold_str = (f"Accuracy: {accuracy:.5f}, "
+                        f"Precision: {precision:.5f},"f" Recall: {recall:.5f}, "
+                        f"F1: {f1:.5f}, ROCAUC: {roc_auc:.5f}, PRAUC: {pr_auc:.5f}")
+
+            result.append(fold_str)
+
+    mean_tpr = np.mean(tprs, axis=0)
+    metrics_per_fold['mean_fpr'] = mean_fpr
+    metrics_per_fold['mean_tpr'] = mean_tpr
+    metrics_per_fold['tprs_'] = tprs
+
+    end_time = time.time()
+    # 计算程序运行时间
+    execution_time = end_time - start_time
+    timestr = "%.3f" % execution_time
+    print("运行时间: " + timestr + " s")
+
+    # 计算五折的平均值
+    avg_accuracy = np.mean(
+        [float(result[0].split(',')[0].split(': ')[1]), float(result[1].split(',')[0].split(': ')[1]),
+         float(result[2].split(',')[0].split(': ')[1]), float(result[3].split(',')[0].split(': ')[1]),
+         float(result[4].split(',')[0].split(': ')[1])])
+    avg_precision = np.mean(
+        [float(result[0].split(',')[1].split(': ')[1]), float(result[1].split(',')[1].split(': ')[1]),
+         float(result[2].split(',')[1].split(': ')[1]), float(result[3].split(',')[1].split(': ')[1]),
+         float(result[4].split(',')[1].split(': ')[1])])
+    avg_recall = np.mean([float(result[0].split(',')[2].split(': ')[1]), float(result[1].split(',')[2].split(': ')[1]),
+                          float(result[2].split(',')[2].split(': ')[1]), float(result[3].split(',')[2].split(': ')[1]),
+                          float(result[4].split(',')[2].split(': ')[1])])
+    avg_f1 = np.mean([float(result[0].split(',')[3].split(': ')[1]), float(result[1].split(',')[3].split(': ')[1]),
+                      float(result[2].split(',')[3].split(': ')[1]), float(result[3].split(',')[3].split(': ')[1]),
+                      float(result[4].split(',')[3].split(': ')[1])])
+    avg_rocauc = np.mean([float(result[0].split(',')[4].split(': ')[1]), float(result[1].split(',')[4].split(': ')[1]),
+                          float(result[2].split(',')[4].split(': ')[1]), float(result[3].split(',')[4].split(': ')[1]),
+                          float(result[4].split(',')[4].split(': ')[1])])
+    avg_prauc = np.mean([float(result[0].split(',')[5].split(': ')[1]), float(result[1].split(',')[5].split(': ')[1]),
+                         float(result[2].split(',')[5].split(': ')[1]), float(result[3].split(',')[5].split(': ')[1]),
+                         float(result[4].split(',')[5].split(': ')[1])])
+
+    print(f"average:")
+    print(f"Accuracy: {avg_accuracy:.5f}, "
+          f"Precision: {avg_precision:.5f},"f" Recall: {avg_recall:.5f}, "
+          f"F1: {avg_f1:.5f}, ROCAUC: {avg_rocauc:.5f}, PRAUC: {avg_prauc:.5f}")
+
+    # 计算参数
+    parameters = {
+        'learning_rate': learning_rate,
+        'model': str(model),
+        'epochs': epochs,
+        'run_time': timestr,
+        'fold1': result[0],
+        'fold2': result[1],
+        'fold3': result[2],
+        'fold4': result[3],
+        'fold5': result[4],
+        '_avg_': f"Accuracy: {avg_accuracy:.5f}, Precision: {avg_precision:.5f}, Recall: {avg_recall:.5f}, F1: {avg_f1:.5f}, ROCAUC: {avg_rocauc:.5f}, PRAUC: {avg_prauc:.5f}",
+        'random_state': random_state,
+        'k': 3
+    }
+    parameters_str = json.dumps(parameters, indent=4)
+    file_path = save_path + 'parameters.txt'
+    if not os.path.exists(os.path.dirname(file_path)):
+        os.makedirs(os.path.dirname(file_path))
+    with open(file_path, 'w') as file:
+        file.write(parameters_str)
+
+    myPlot(metrics_per_fold, save_img_path, save_csv_path)
+
+
+if __name__ == '__main__':
+    epochs = 1500
+    learning_rate = 1e-4
+
+    # save result config
+    t = time.localtime()
+    time_index = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    save_path = './Result/' + time_index + '/'
+    print('\n' + save_path)
+
+    # 创建保存的目录(日期)
+    os.mkdir(save_path)
+    os.mkdir(save_path + 'plot')
+    os.mkdir(save_path + 'pred')
+    save_img_path = save_path + 'plot' + '/'  # Result/xxx/plot
+    os.mkdir(save_img_path + 'imgs')
+    os.mkdir(save_img_path + 'csv')
+    save_csv_path = save_img_path + 'csv' + '/'  # plot/csv
+    save_img_path = save_img_path + 'imgs' + '/'  # plot/imgs
+
+    train(myModel=PMDGGM, epochs=epochs, learning_rate=learning_rate,
+          save_path=save_path, save_img_path=save_img_path, save_csv_path=save_csv_path)
---
+++ utils.py
@@ -0,0 +1,296 @@
+# -*- coding:utf-8 -*-
+# 作者: 王业强__
+# 日期: 2024-08-15
+# 声明: Welcome my coding environments!
+
+import os
+import torch
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+
+
+
+def load_data():
+
+    # 加载 M,D,M-D 矩阵
+    M = pd.read_csv('Datasets/MS.csv', header=None).values
+    D = pd.read_csv('Datasets/DS.csv', header=None).values
+    M_D = pd.read_csv('Datasets/M-D.csv', header=None).values
+
+    # 加载训练用到的正负样本
+    all_samples = pd.read_csv('Datasets/samples/samples.csv').values
+    labels = pd.read_csv('Datasets/samples/labels.csv').values
+
+    all_samples = torch.tensor(all_samples, dtype=torch.long)
+    labels = torch.tensor(labels.squeeze(), dtype=torch.float)
+    # print("all_samples.shape: ", all_samples.shape)
+    # print("labels.shape: ", labels.shape)
+
+    # 处理矩阵
+    M = torch.tensor(M, dtype=torch.float)
+    D = torch.tensor(D, dtype=torch.float)
+    M_D = torch.tensor(M_D, dtype=torch.float)
+    D_M = M_D.T
+    combined_matrix = torch.cat((
+        torch.cat((M, M_D), dim=1),
+        torch.cat((D_M, D), dim=1)
+    ), dim=0)
+
+    # 读取特征矩阵
+    features = pd.read_excel('./Datasets/features/features.xlsx', header=None).values
+
+    scaler = StandardScaler()
+    features = scaler.fit_transform(features)
+    features = torch.tensor(features, dtype=torch.float)
+    features = torch.nan_to_num(features, nan=0.0)
+    features = (features > 0).float()
+
+    # 构建边索引
+    edge_index = (M_D > 0).nonzero(as_tuple=False).t().contiguous()
+    edge_index[1, :] += M_D.shape[0]  # 修正边索引
+    edge_index_else = edge_index.flip(dims=[0])
+    edge_index = torch.cat([edge_index, edge_index_else], dim=1)
+    edge_attr = torch.ones(edge_index.shape[1])
+
+    return features, edge_index, edge_attr, all_samples, labels
+
+# 五折交叉验证重新计算边集
+def get_MD(edge_index):
+    edge_index = edge_index.T
+    num_miRNAs = 812
+    num_diseases = 438
+
+    M_D = np.zeros((num_miRNAs, num_diseases))
+    D_M = np.zeros((num_diseases, num_miRNAs))
+
+    for miRNA_idx, disease_idx in edge_index:
+        M_D[miRNA_idx, disease_idx - 812] = 1
+        D_M[disease_idx - 812, miRNA_idx] = 1
+
+    # 计算训练集上新的边集
+    # 读取需要融合的相似性矩阵
+    miRNA_cosine_sim = cosine_similarity(M_D)  # 功能相似性
+    miRNA_lnc_sim = pd.read_csv('Datasets/similarly/M_lnc.csv', header=None).values   # lncRNA相似性
+    miRNA_gene_sim = pd.read_csv('Datasets/similarly/M_gene.csv', header=None).values # gene相似性
+
+    disease_jaccard_sim = calculate_jaccard_similarity(D_M)  # 功能相似性
+    disease_lnc_sim = pd.read_csv('Datasets/similarly/D_lnc.csv', header=None).values   # lncRNA相似性
+    disease_gene_sim = pd.read_csv('Datasets/similarly/D_gene.csv', header=None).values # gene相似性
+    disease_mesh_sim = pd.read_csv('Datasets/similarly/D_mesh.csv', header=None).values # 语义相似性
+
+    M = 0.5 * miRNA_cosine_sim + 0.25 * miRNA_lnc_sim + 0.25 * miRNA_gene_sim
+    D = 0.4 * disease_jaccard_sim + 0.2 * disease_mesh_sim + 0.2 * disease_lnc_sim + 0.2 * disease_gene_sim
+
+    M = np.array(M)
+    D = np.array(D)
+
+    # 得到邻接矩阵
+    M = [[1 if x > 0.12 and (j != i) else 0 for i, x in enumerate(row)] for j, row in enumerate(M)]
+    D = [[1 if x > 0.12 and (j != i) else 0 for i, x in enumerate(row)] for j, row in enumerate(D)]
+
+    return torch.tensor(M, dtype=torch.float), torch.tensor(D, dtype=torch.float)
+
+
+def calculate_jaccard_similarity(matrix):
+    size = matrix.shape[0]
+    jaccard_sim = np.zeros((size, size))
+    for i in range(size):
+        for j in range(i, size):
+            intersection = np.logical_and(matrix[i], matrix[j]).sum()
+            union = np.logical_or(matrix[i], matrix[j]).sum()
+            jaccard_sim[i, j] = jaccard_sim[j, i] = intersection / union if union != 0 else 0
+    return jaccard_sim
+
+
+def save_xlsx_data(metrics_per_fold, save_csv_path):
+    df_plot = pd.DataFrame([metrics_per_fold['epoch_losses'],
+                            metrics_per_fold['epoch_accuracies'],
+                            metrics_per_fold['roc_aucs'],
+                            metrics_per_fold['accuracies'],
+                            metrics_per_fold['precisions'],
+                            metrics_per_fold['recalls'],
+                            metrics_per_fold['f1s'],
+                            metrics_per_fold['fprs'],
+                            metrics_per_fold['tprs'],
+                            metrics_per_fold['mean_fpr'],
+                            metrics_per_fold['mean_tpr'],
+                            metrics_per_fold['tprs_'],
+                            metrics_per_fold['std_tpr'],
+                            metrics_per_fold['pr_precisions'],
+                            metrics_per_fold['pr_recalls'],
+                            metrics_per_fold['pr_aucs']],
+
+                           index=['Train_Loss', 'Train_accuracy', 'Roc_Auc', 'Accuracy', 'Precision', 'Recall',
+                                  'F1_Score', 'fprs', 'tprs', 'mean_fpr', 'mean_tpr', 'tprs_', 'std_tpr',
+                                  'pr_precisions', 'pr_recalls', 'pr_aucs'])
+
+    df_plot.to_excel(save_csv_path + 'plot.xlsx', index=True, header=False)
+    df = pd.DataFrame(metrics_per_fold['tprs_'])
+    df.to_csv(save_csv_path + 'tprs_.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['mean_tpr'])
+    df.to_csv(save_csv_path + 'mean_tpr.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['mean_fpr'])
+    df.to_csv(save_csv_path + 'mean_fpr.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['epoch_losses'])
+    df.to_csv(save_csv_path + 'epoch_losses.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['epoch_accuracies'])
+    df.to_csv(save_csv_path + 'epoch_accuracies.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['roc_aucs'])
+    df.to_csv(save_csv_path + 'roc_aucs.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['accuracies'])
+    df.to_csv(save_csv_path + 'accuracies.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['precisions'])
+    df.to_csv(save_csv_path + 'precisions.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['recalls'])
+    df.to_csv(save_csv_path + 'recalls.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['f1s'])
+    df.to_csv(save_csv_path + 'f1s.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['pr_precisions'])
+    df.to_csv(save_csv_path + 'pr_precisions.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['pr_recalls'])
+    df.to_csv(save_csv_path + 'pr_recalls.csv', index=False)
+    df = pd.DataFrame(metrics_per_fold['pr_aucs'])
+    df.to_csv(save_csv_path + 'pr_aucs.csv', index=False)
+
+
+# 绘制组合图像
+def myPlot(metrics_per_fold, save_img_path, save_csv_path):
+    # 保存绘图数据
+    save_xlsx_data(metrics_per_fold, save_csv_path)
+
+    fig, axes = plt.subplots(2, 3, figsize=(18, 12), constrained_layout=True)
+
+    # 设置图表风格
+    plt.style.use('classic')
+
+    # 绘制ROC曲线
+    for i, (roc_auc, fpr, tpr) in enumerate(zip(metrics_per_fold['roc_aucs'], metrics_per_fold['fprs'],
+                                                metrics_per_fold['tprs'])):
+        axes[0, 0].plot(fpr, tpr, lw=1, alpha=0.8, label=f'Fold {i + 1} AUC={roc_auc:.4f}')
+    axes[0, 0].plot([0, 1], [0, 1], 'k--')
+    mean_tpr = metrics_per_fold['mean_tpr']
+    mean_tpr[-1] = 1.0
+    std_tpr = np.std(metrics_per_fold['tprs_'], axis=0)
+    axes[0, 0].set_xlim([-0.05, 1.05])
+    axes[0, 0].set_ylim([-0.05, 1.05])
+    axes[0, 0].set_xlabel('False_Positive_Rate', fontsize=15)
+    axes[0, 0].set_ylabel('True_Positive_Rate', fontsize=15)
+    axes[0, 0].set_title('ROC Curves for Each Fold', fontsize=18)
+    axes[0, 0].legend(loc="lower right", fontsize=12)
+
+    metrics_titles = ['Roc_Auc', 'Accuracy', 'Precision', 'Recall', 'F1_Score']
+    metrics_keys = ['roc_aucs', 'accuracies', 'precisions', 'recalls', 'f1s']
+
+    colors = ['blue', 'green', 'red', 'purple', 'orange']
+    plt.style.use('classic')
+    for i, (metric_key, title) in enumerate(zip(metrics_keys, metrics_titles)):
+        ax = axes.flatten()[i + 1]
+        ax.plot(range(1, 6), metrics_per_fold[metric_key], marker='o', linestyle='-', color=colors[i], label=title)
+        # 平均线
+        mean_value = np.mean(metrics_per_fold[metric_key])
+        ax.axhline(mean_value, color=colors[i], linestyle='--', label='Mean')
+        # 调整纵轴范围
+        min_val, max_val = min(metrics_per_fold[metric_key]), max(metrics_per_fold[metric_key])
+        ax.set_ylim(min_val * 0.95, max_val * 1.05)
+
+        ax.set_title(title, fontsize=30)
+        ax.set_xlabel('Fold', fontsize=15)
+        ax.set_xticks(range(1, 6))
+        ax.set_xticklabels([f'Fold {j}' for j in range(1, 6)], fontsize=14)
+        ax.set_ylabel(title, fontsize=15)
+        ax.grid(True, which='both', linestyle='--', linewidth=0.5)
+        ax.legend(fontsize=12)
+
+    fig.savefig(save_img_path + 'Validating_Indexes.png')
+    plt.show()
+
+    fig_train, axes = plt.subplots(1, 2, figsize=(24, 12), constrained_layout=True)
+
+    # 绘制LOSS曲线
+    for i, fold_loss in enumerate(metrics_per_fold['epoch_losses']):
+        axes[0].plot(range(1, len(fold_loss) + 1), fold_loss, label=f'Fold {i + 1}')
+    axes[0].set_title('Training_Loss', fontsize=40)
+    axes[0].set_xlabel('Epoch', fontsize=30)
+    axes[0].set_ylabel('Loss', fontsize=30)
+    axes[0].legend(loc='upper right', fontsize=30)
+
+    # 绘制ACC曲线
+    for i, fold_acc in enumerate(metrics_per_fold['epoch_accuracies']):
+        axes[1].plot(range(1, len(fold_acc) + 1), fold_acc, label=f'Fold {i + 1}')
+    axes[1].set_title('Training_Acc', fontsize=40)
+    axes[1].set_xlabel('Epoch', fontsize=30)
+    axes[1].set_ylabel('Acc', fontsize=30)
+    axes[1].legend(loc='lower right', fontsize=30)
+
+    fig_train.savefig(save_img_path + 'Training_Indexes.png')  # 保存图像
+    plt.show()
+
+    save_individual_metrics(metrics_per_fold, save_img_path)
+
+# 绘制单个图像
+def save_individual_metrics(metrics_per_fold, save_img_path):
+    metrics_titles = ['ROC AUC', 'Accuracy', 'Precision', 'Recall', 'F1 Score', 'Train Accuracy']
+    metrics_keys = ['roc_aucs', 'accuracies', 'precisions', 'recalls', 'f1s', 'epoch_accuracies']
+    colors = ['blue', 'green', 'red', 'purple', 'orange', 'brown']
+
+    plt.style.use('classic')
+
+    # 绘制ROC曲线
+    fig, ax = plt.subplots(figsize=(8, 8))
+    for i, (roc_auc, fpr, tpr) in enumerate(zip(metrics_per_fold['roc_aucs'], metrics_per_fold['fprs'],
+                                                metrics_per_fold['tprs'])):
+        ax.plot(fpr, tpr, lw=1, alpha=0.8, label=f'Fold {i + 1} AUC={roc_auc:.4f}')
+    ax.plot([0, 1], [0, 1], 'k--')
+    mean_tpr = metrics_per_fold['mean_tpr']
+    mean_tpr[-1] = 1.0
+    std_tpr = np.mean(metrics_per_fold['tprs_'], axis=0)
+    ax.set_xlim([-0.05, 1.05])
+    ax.set_ylim([-0.05, 1.05])
+    ax.set_title('ROC Curves for Each Fold', fontsize=24)
+    ax.set_xlabel('False Positive Rate', fontsize=18)
+    ax.set_ylabel('True Positive Rate', fontsize=18)
+    ax.legend(loc="lower right", fontsize=12)
+    fig.savefig(os.path.join(save_img_path, 'ROC_Curves.png'))
+
+    # 绘制PR曲线
+    fig, ax = plt.subplots(figsize=(8, 8))
+    for i, (precision, recall, pr_auc) in enumerate(
+            zip(metrics_per_fold['pr_precisions'], metrics_per_fold['pr_recalls'],
+                metrics_per_fold['pr_aucs'])):
+        ax.plot(recall, precision, lw=1, alpha=0.8, label=f'Fold {i + 1} PR AUC={pr_auc:.4f}')
+    ax.plot([0, 1], [1, 0], 'k--')
+    ax.set_title('PR Curves for Each Fold', fontsize=24)
+    ax.set_xlim([-0.05, 1.05])
+    ax.set_ylim([-0.05, 1.05])
+    ax.set_xlabel('Recall', fontsize=18)
+    ax.set_ylabel('Precision', fontsize=18)
+    ax.legend(loc="lower left", fontsize=12)
+    fig.savefig(os.path.join(save_img_path, 'PR_Curves.png'))
+
+
+    plt.style.use('classic')
+    # 绘制其他评价指标
+    for i, (metric_key, title) in enumerate(zip(metrics_keys, metrics_titles)):
+        fig, ax = plt.subplots(figsize=(8, 8))
+        values = [np.mean(fold) for fold in metrics_per_fold[metric_key]] if metric_key == 'epoch_accuracies' else \
+        metrics_per_fold[metric_key]
+        ax.plot(range(1, 6), values, marker='o', linestyle='-', color=colors[i % len(colors)], label=title)
+        mean_value = np.mean(values)
+        ax.axhline(mean_value, color=colors[i % len(colors)], linestyle='--', label='Mean')
+        ax.set_ylim(min(values) * 0.95, max(values) * 1.05)
+        ax.set_title(title, fontsize=30)
+        ax.set_xlabel('Fold', fontsize=16)
+        ax.set_xticks(range(1, 6))
+        ax.set_xticklabels([f'Fold {j}' for j in range(1, 6)])
+        ax.set_ylabel(title, fontsize=16)
+        ax.grid(True, which='both', linestyle='--', linewidth=0.5)
+        ax.legend(fontsize=12)
+        plt.savefig(os.path.join(save_img_path, f'{title.replace(" ", "_")}.png'))
+        plt.close()
+
